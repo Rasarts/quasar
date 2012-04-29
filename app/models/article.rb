f66db8096ca181d7b153bb3aac78674dbbaf161a
Vladimir Melnick <egotraumatic@gmail.com>
2012-04-29 13:44:10 +0300

    article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,12 @@
 class Article < ActiveRecord::Base
   attr_accessible :content, :description, :status, :title, :user_id
+    
+  STATUSES= %w[ published moderated in_moderation in_work removed ]
+
+  # validations
+  validates :title,   presence: true
+  validates :content, presence: true
+  validates :status,  presence: true, inclusion: { in: STATUSES }
+  validates :user_id, presence: true
+  
 end
